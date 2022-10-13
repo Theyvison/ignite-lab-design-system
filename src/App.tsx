fe15ec3ae604a9c948c18722ebfa91ba3a718033
Theyvison <66499852+Theyvison@users.noreply.github.com>
2022-10-13 15:41:48 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className='text-2xl text-cyan-500'>Hello world</h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded hover:bg-cyan-300'>Entrar</button>
+    </>
+  )
+}
---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Text, TextProps } from "./text";
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'lorem',
+        size: 'md'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {
+    args: {
+
+    }
+}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Text with p tag</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        },
+        asChild: {
+            table: {
+                disable: true
+            }
+        }
+    }
+}
